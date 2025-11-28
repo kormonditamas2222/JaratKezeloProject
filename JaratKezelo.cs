674c077e87a622ca7624f8d59622dc5c07c74f34
kormonditamas2222 <t.kormond2006@gmail.com>
2025-11-28 13:45:21 +0100

    ujarat, keses

--- JaratKezelo.cs
+++ JaratKezelo.cs
@@ -3,5 +3,38 @@
     public class JaratKezelo
     {
         private List<Jarat> jaratok;
+
+        public void ujJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            Jarat jarat = new(jaratSzam, repterHonnan, repterHova, indulas, 0);
+            foreach (var item in jaratok)
+            {
+                if (item.JaratSzam == jarat.JaratSzam)
+                {
+                    throw new ArgumentException("A járatszámnak egyedinek kell lennie!");
+                }
+            }
+            jaratok.Add(jarat);
+        }
+        public void Keses(string jaratSzam, int keses)
+        {
+            foreach (var item in jaratok)
+            {
+                if (item.JaratSzam == jaratSzam)
+                {
+                    if (item.Keses-keses < 0)
+                    {
+                        throw new NegativKesesException(item.Keses - keses);
+                    }
+                    item.Keses += keses;
+                }
+            }
+        }
+    }
+    class NegativKesesException : Exception
+    {
+        public NegativKesesException(int osszKeses) : base("Nem lehet negatív késés érték: " + osszKeses)
+        {
+        }
     }
 }
